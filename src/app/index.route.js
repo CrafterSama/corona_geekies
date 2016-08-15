c5f63a5959b142b8c9b9d0a1100b8c239eb6688c
edgar <eespinetti_99@hotmail.com>
2016-08-14 23:20:40 -0430

    cambio en el input password autocompleter=off

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -2,56 +2,54 @@
   'use strict';
 
   angular
-    .module('appCorona')
-    .config(routerConfig);
+      .module('appCorona')
+      .config(routerConfig);
 
   /** @ngInject */
   function routerConfig($stateProvider, $urlRouterProvider, $locationProvider) {
 
-    function loginRequired($q, $location, $auth) {
-      var deferred = $q.defer();
+      function loginRequired($q, $location, $auth) {
+        var deferred = $q.defer();
 
-      if ($auth.isAuthenticated()) {
-
-        deferred.resolve();
-      } else {
-
-        $location.path('/login');
-      }
-      return deferred.promise;
-    }
-
-
-    $stateProvider
-      .state('main', {
-        url: '/main',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainController',
-        controllerAs: 'main',
-        resolve: {
-          loginRequired: loginRequired
+        if ($auth.isAuthenticated()) {
+          deferred.resolve();
+        } else {
+          $location.path('/login');
         }
-      })
-      .state('login', {
-        url: '/login',
-        templateUrl: 'app/login/login.html',
-        controller: 'LoginController',
-        controllerAs: 'login'
-      })
-      .state('register', {
-        url: '/register',
-        templateUrl: 'app/register/register.html',
-        controller: 'RegisterController',
-        controllerAs: 'register'
-      })
-      .state('home', {
-        url: '/home',
-        templateUrl: 'app/home/home.html',
-        controller: 'HomeController',
-        controllerAs: 'home'
-      });
+        return deferred.promise;
+      }
 
-    $urlRouterProvider.otherwise('/home');
+
+      $stateProvider
+          .state('main', {
+            url: '/main',
+            templateUrl: 'app/main/main.html',
+            controller: 'MainController',
+            controllerAs: 'main',
+            resolve: {
+              loginRequired: loginRequired
+            }
+          })
+          .state('login', {
+            url: '/login',
+            templateUrl: 'app/login/login.html',
+            controller: 'LoginController',
+            controllerAs: 'login'
+          })
+          .state('register', {
+            url: '/register',
+            templateUrl: 'app/register/register.html',
+            controller: 'RegisterController',
+            controllerAs: 'register'
+          })
+          .state('home', {
+            url: '/home',
+            templateUrl: 'app/home/home.html',
+            controller: 'HomeController',
+            controllerAs: 'home'
+          });
+  
+      $urlRouterProvider.otherwise('/home');
 
   }
 
