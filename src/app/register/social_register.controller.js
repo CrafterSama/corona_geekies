bd5315ba9d9bcb012a53c30a5d6e927592ad4b41
Edgar Espinetti <eespinetti@4geeks.com.ve>
2016-08-18 16:54:00 -0400

    Validando ciudad en el registro con facebook

--- src/app/register/social_register.controller.js
+++ src/app/register/social_register.controller.js
@@ -2,83 +2,91 @@
  * Created by edgar on 04/08/16.
  */
 (function() {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('appCorona')
-    .controller('SocialRegisterController', SocialRegisterController);
+    angular
+        .module('appCorona')
+        .controller('SocialRegisterController', SocialRegisterController);
 
-  /** @ngInject */
-  function SocialRegisterController($scope, $auth, $state, $http, toastr) {
-    var vm = this;
-    vm.server = [];
-    vm.submitForm = function(form) {
-      console.log(form);
-      vm.submitted = true;
+    /** @ngInject */
+    function SocialRegisterController($scope, $auth, $state, $http, toastr) {
+        var vm = this;
+        vm.server = [];
 
-      // check to make sure the form is completely valid
-      if (form.$valid) {
+        $http.get('http://graph.facebook.com/v2.2/me')
+            .then(function(result){
+                console.log(result);
+            }, function(result){
 
-          var date = vm.user.birthdate;
-          var pieces = date.split('-');
-          pieces.reverse();
-          var reversed = pieces.join('-');
+            });
 
-          var phone = vm.user.phone_cel.replace(/[.*+?^${}()|[\]\\-]/g, "");
-          phone = phone.replace(' ', '');
+        vm.submitForm = function(form) {
+            console.log(form);
+            vm.submitted = true;
 
+            // check to make sure the form is completely valid
+            if (form.$valid) {
 
-        $auth.signup({
-            username:   vm.user.alias,
-            password:   vm.user.password,
-            city:       vm.user.city,
-            birthdate:  reversed,
-            first_name: vm.user.first_name,
-            last_name:  vm.user.last_name,
-            telephone:  phone,
-            email:      vm.user.email
-          })
-          .then(function() {
-            // Si se ha registrado correctamente,
-            // Podemos redirigirle a otra parte
-            $state.go('login');
-          })
-          .catch(function(response) {
-            vm.server = response.data;
-            // Si ha habido errores, llegaremos a esta función
-          });
-      }
+                var date = vm.user.birthdate;
+                var pieces = date.split('-');
+                pieces.reverse();
+                var reversed = pieces.join('-');
 
-    };
+                var phone = vm.user.phone_cel.replace(/[.*+?^${}()|[\]\\-]/g, "");
+                phone = phone.replace(' ', '');
 
 
+                $auth.signup({
+                        username:   vm.user.alias,
+                        password:   vm.user.password,
+                        city:       vm.user.city,
+                        birthdate:  reversed,
+                        first_name: vm.user.first_name,
+                        last_name:  vm.user.last_name,
+                        telephone:  phone,
+                        email:      vm.user.email
+                    })
+                    .then(function() {
+                        // Si se ha registrado correctamente,
+                        // Podemos redirigirle a otra parte
+                        $state.go('login');
+                    })
+                    .catch(function(response) {
+                        vm.server = response.data;
+                        // Si ha habido errores, llegaremos a esta función
+                    });
+            }
+
+        };
 
 
-    $http.get(
-      'https://graph.facebook.com/me?access_token='+localStorage.getItem("token_facebook")+'')
-      .then(function(result){
-          var city = "";
-          var pieces = "";
-          if(result.data.hometown !== undefined){city = result.data.hometown.name}else{ city = "";}
-          if(result.data.birthday !== undefined){var pieces = result.data.birthday.split('/'); pieces.join('-') }else{ pieces = "";}
 
-        console.log(result.data);
-        vm.user = {};
-        vm.user.email = result.data.email;
-        vm.user.ciudad = city;
-        vm.user.last_name = result.data.last_name;
-        vm.user.first_name = result.data.first_name;
-        vm.user.birthdate = pieces;
-        vm.user.image_url = 'http://graph.facebook.com/'+result.data.id+'/picture?type=large';
-      }, function(result){
-        console.log(result)
-      });
 
-      $http.get('graph.facebook.com/v2.2/me')
-          .then(function(result){
-              console.log(result);
-          }, function(result){
+        $http.get(
+                'https://graph.facebook.com/me?access_token='+localStorage.getItem("token_facebook")+'')
+            .then(function(result){
+                var city = "";
+                var pieces = "";
+                if(result.data.hometown !== undefined){city = result.data.hometown.name}else{ city = "";}
+                if(result.data.birthday !== undefined){var pieces = result.data.birthday.split('/'); pieces.join('-') }else{ pieces = "";}
 
-          });
-  }
+                console.log(result.data);
+                vm.user = {};
+                vm.user.email = result.data.email;
+                vm.user.ciudad = city;
+                vm.user.last_name = result.data.last_name;
+                vm.user.first_name = result.data.first_name;
+                vm.user.birthdate = pieces;
+                vm.user.image_url = 'http://graph.facebook.com/'+result.data.id+'/picture?type=large';
+            }, function(result){
+                console.log(result)
+            });
+
+        $http.get('https://graph.facebook.com/me?fields=id,name,birthday?access_token='+localStorage.getItem("token_facebook")+'')
+            .then(function(result){
+                console.log(result);
+            }, function(result){
+
+            });
+    }
 })();
