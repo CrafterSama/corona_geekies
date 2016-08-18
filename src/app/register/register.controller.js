625e0498566574f535cd486c6c3c0a36ff16a9dd
Edgar Espinetti <eespinetti@4geeks.com.ve>
2016-08-18 14:35:25 -0400

    perfil

--- src/app/register/register.controller.js
+++ src/app/register/register.controller.js
@@ -9,9 +9,10 @@
     .controller('RegisterController', RegisterController);
 
   /** @ngInject */
-  function RegisterController($scope, $auth, $state, $http) {
+  function RegisterController($scope, $auth, $state, $http, toastr) {
     var vm = this;
     vm.idGuerrero = false;
+    vm.user = {};
 
 
     vm.server = [];
@@ -31,7 +32,7 @@
       // check to make sure the form is completely valid
       if (form.$valid) {
 
-          var date = vm.user.birthday;
+          var date = vm.user.birthdate;
           var pieces = date.split('-');
           pieces.reverse();
           var reversed = pieces.join('-');
@@ -65,6 +66,38 @@
 
     vm.registerGuerrero = function(){
         vm.idGuerrero = !vm.idGuerrero;
+    };
+
+    vm.postGuerrero = function(id) {
+        if (id !== undefined) {
+            var value = {id_guerrero: vm.user.guerrero};
+
+            $http({
+                method: 'POST',
+                url: 'http://dev.corona.geekies.co:8000/accounts/id_guerrero/',
+                data: value
+            }).then(function successCallback(response) {
+
+                var birthdate = response.data.user.birthdate;
+
+                var pieces = birthdate.split('-');
+                pieces.reverse();
+                var reversed = pieces.join('-');
+
+                vm.user = response.data.user;
+                vm.user.birthdate = birthdate;
+                vm.user.password = "";
+                // this callback will be called asynchronously
+                // when the response is available
+            }, function errorCallback(response) {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                toastr.error('ID Guerrero no Existe');
+                vm.registerGuerrero();
+            });
+
+
+        };
     }
 
   }
